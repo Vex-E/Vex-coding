d4e88ee6c15c9d01be0e5a5bc5400517a54f3277
Dylan Mercado <dmercado_23@ESUSD.esusd.k12.ca.us>
2020-03-13 15:13:46 -0700

    I fix a bug where one wheel when backwards.

--- Motor coding.c
+++ Motor coding.c
@@ -2,7 +2,7 @@
 #pragma config(Sensor, in2,    Back,           sensorLineFollower)
 #pragma config(Sensor, dgtl1,  button,         sensorTouch)
 #pragma config(Sensor, dgtl2,  button2,        sensorTouch)
-#pragma config(Motor,  port2,           backright,     tmotorVex269_MC29, openLoop)
+#pragma config(Motor,  port2,           backright,     tmotorVex269_MC29, openLoop, reversed)
 #pragma config(Motor,  port3,           topleft,       tmotorVex269_MC29, openLoop)
 #pragma config(Motor,  port4,           topright,      tmotorVex269_MC29, openLoop)
 #pragma config(Motor,  port5,           backleft,      tmotorVex269_MC29, openLoop)
@@ -14,13 +14,13 @@ while(1==1)
 {
 	if(SensorValue(button)==1)
 {
-   startMotor(topright, 100);
+   startMotor(topright,100);
 }
-startMotor(topleft, 100);
+   startMotor(topleft,100);
 {
-startMotor(backleft, 100);
+   startMotor(backleft,100);
 }
-startMotor(backright, 100);
+   startMotor(backright,100);
 // above is the autonomus //
 }
 
